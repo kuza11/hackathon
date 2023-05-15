14f72f9a13cba37aaeff0856227a820ce58a2710
zvolaja <zvolaja@student.uzlabina.cz>
2023-05-15 10:53:40 +0200

    db

--- hackathon_web/src/pages/api/hello.js
+++ hackathon_web/src/pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { InfluxDB, Point } from '@influxdata/influxdb-client'
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const queryApi = new InfluxDB({YOUR_URL, YOUR_API_TOKEN}).getQueryApi(YOUR_ORG);
+  res.status(200).json({ name: 'John Doe' });
 }

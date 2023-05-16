c07f7fec7a3f0c4b624bf93025709006bdf5e1d8
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-16 20:41:00 +0200

    Update index.js

--- hackathon_backend/index.js
+++ hackathon_backend/index.js
@@ -39,23 +39,23 @@ client.on('connect', () => {
 
 
         // Publish a message
-        const topic = 'senzor/console/log';
-        const message = JSON.stringify({
-            device: 'your_device',
-            timestamp: 123456789,
-            sensor_top: 420,
-            sensor_bottom: 69,
-            angle: 20.69
-        });
-        client.publish(topic, message, (err) => {
-            if (err) {
-                console.error('Failed to publish message:', err);
-            } else {
-                console.log('Message published');
-            }
-            // Disconnect the MQTT client
-            client.end();
-        });
+        // const topic = 'senzor/console/log';
+        // const message = JSON.stringify({
+        //     device: 'your_device',
+        //     timestamp: 123456789,
+        //     sensor_top: 420,
+        //     sensor_bottom: 69,
+        //     angle: 20.69
+        // });
+        // client.publish(topic, message, (err) => {
+        //     if (err) {
+        //         console.error('Failed to publish message:', err);
+        //     } else {
+        //         console.log('Message published');
+        //     }
+        //     // Disconnect the MQTT client
+        //     client.end();
+        // });
 });
 
 client.on('message', (topic, message) => {
@@ -77,34 +77,48 @@ client.on('message', (topic, message) => {
             if (err) {
                 console.log('Message could not be saved in the database');
             } else {
+                //add new device
                 console.log('Message saved to the database');
+                try {
+                    // Save the message to the database
+                    const query = "INSERT INTO devices (mac, angle) VALUES (?, ?)";
+                    db.query(query, [device, angle], (err, result) => {
+                        if (err) {
+                           console.log('Message could not be saved in the database');
+                        } else {
+                            console.log('Message saved to the database');
+                        }
+                        });
+                    } catch (error) {
+                    console.log('Error parsing JSON message:', error.message);
+                }
             }
         });
       } catch (error) {
         console.log('Error parsing JSON message:', error.message);
       }
     }
-  });
+});
   
 
-client.on('senzor/console/log', (topic, message) => {
-    console.log('Received message on senzor/console/log');
-    console.log('Message:', message.toString());
+// client.on('senzor/console/log', (topic, message) => {
+//     console.log('Received message on senzor/console/log');
+//     console.log('Message:', message.toString());
 
-    const msgObj = JSON.parse(message.toString());
-    const device = msgObj.device;
-    const timestamp = msgObj.timestamp;
-    const sensorTop = msgObj.sensor_top;
-    const sensorBottom = msgObj.sensor_bottom;
-    const angle = msgObj.angle;
+//     const msgObj = JSON.parse(message.toString());
+//     const device = msgObj.device;
+//     const timestamp = msgObj.timestamp;
+//     const sensorTop = msgObj.sensor_top;
+//     const sensorBottom = msgObj.sensor_bottom;
+//     const angle = msgObj.angle;
 
-    // Save the message to the database
-    const query = "INSERT INTO data (time, mac, sens_top, sens_bottom) VALUES (?, ?, ?, ?)";
-    db.query(query, [timestamp, device, sensorTop, sensorBottom], (err, result) => {
-        if (err) {
-            console.log('Message could not be saved in the database');
-        } else {
-            console.log('Message saved to the database');
-        }
-    });
-});
+//     // Save the message to the database
+//     const query = "INSERT INTO data (time, mac, sens_top, sens_bottom) VALUES (?, ?, ?, ?)";
+//     db.query(query, [timestamp, device, sensorTop, sensorBottom], (err, result) => {
+//         if (err) {
+//             console.log('Message could not be saved in the database');
+//         } else {
+//             console.log('Message saved to the database');
+//         }
+//     });
+// });

534ecca6c7216eafca54568a859ea3c321b36a5a
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-17 00:28:41 +0200

    Update main.py

--- hackathon_backend/main.py
+++ hackathon_backend/main.py
@@ -9,15 +9,14 @@ password = "senzor12345"
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT broker!")
-        # Publish a message
-        topic = "senzor/console/log"
-        message = "{\"device\": \"your_device\", \"timestamp\": 123456789, \"sensor_top\": 420, \"sensor_bottom\": 69, \"angle\": 20.69}"
-        client.publish(topic, message)
+        # Subscribe to the desired topic
+        client.subscribe("senzor/console/log")
     else:
         print("Failed to connect, return code %d" % rc)
 
 def on_message(client, userdata, msg):
-    print("Received message: %s" % msg.payload.decode())
+    print("Received message on topic: %s" % msg.topic)
+    print("Message payload: %s" % msg.payload.decode())
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -26,10 +25,5 @@ client.on_message = on_message
 client.username_pw_set(username, password)
 
 client.connect(broker_address, broker_port)
-
-def subscribe_topic(topic):
-    client.subscribe(topic)
-
-subscribe_topic("B8:D6:1A:43:88:A8/data")
 
 client.loop_forever()

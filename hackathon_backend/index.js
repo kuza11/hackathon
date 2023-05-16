2dcd6df51b9dbdc2105ed68b15bbbe94c7519697
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-16 13:01:18 +0200

    Update index.js

--- hackathon_backend/index.js
+++ hackathon_backend/index.js
@@ -5,7 +5,7 @@ const mqtt = require('mqtt');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    port: 3306,
+    port: 3307,
     password: 'ZlabSuckDick',
     database: 'hackathon'
 });

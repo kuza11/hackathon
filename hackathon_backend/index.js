dd22977be09f8785f571a495333330ffc8dd8a1c
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-16 20:53:20 +0200

    Update index.js

--- hackathon_backend/index.js
+++ hackathon_backend/index.js
@@ -39,23 +39,23 @@ client.on('connect', () => {
 
 
         // Publish a message
-        const topic = 'senzor/console/log';
-        const message = JSON.stringify({
-            device: 'your_device',
-            timestamp: 123456789,
-            sensor_top: 420,
-            sensor_bottom: 69,
-            angle: 20.69
-        });
-        client.publish(topic, message, (err) => {
-            if (err) {
-                console.error('Failed to publish message:', err);
-            } else {
-                console.log('Message published');
-            }
-            // Disconnect the MQTT client
-            client.end();
-        });
+        // const topic = 'senzor/console/log';
+        // const message = JSON.stringify({
+        //     device: 'your_device',
+        //     timestamp: 123456789,
+        //     sensor_top: 420,
+        //     sensor_bottom: 69,
+        //     angle: 20.69
+        // });
+        // client.publish(topic, message, (err) => {
+        //     if (err) {
+        //         console.error('Failed to publish message:', err);
+        //     } else {
+        //         console.log('Message published');
+        //     }
+        //     // Disconnect the MQTT client
+        //     client.end();
+        // });
 });
 
 client.on('message', (topic, message) => {
@@ -75,23 +75,9 @@ client.on('message', (topic, message) => {
         const query = "INSERT INTO data (time, mac, sens_top, sens_bottom) VALUES (?, ?, ?, ?)";
         db.query(query, [timestamp, device, sensorTop, sensorBottom], (err, result) => {
             if (err) {
-                console.log('Log could not be saved in the databasee');
+                console.log('Message could not be saved in the database');
             } else {
-                //add new device
-                console.log('Log saved to the database');
-                try {
-                    // Save the message to the database
-                    const query = "INSERT INTO devices (mac, angle) VALUES (?, ?)";
-                    db.query(query, [device, angle], (err, result) => {
-                        if (err) {
-                           console.log('Device could not be saved in the database');
-                        } else {
-                            console.log('Device saved to the database');
-                        }
-                        });
-                    } catch (error) {
-                    console.log('Error parsing JSON message:', error.message);
-                }
+                console.log('Message saved to the database');
             }
         });
       } catch (error) {

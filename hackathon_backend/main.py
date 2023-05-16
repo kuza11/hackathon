f38f10365a9d7a04a6a2ee735a13cbf8c839bd40
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-17 00:04:35 +0200

    Update main.py

--- hackathon_backend/main.py
+++ hackathon_backend/main.py
@@ -9,18 +9,11 @@ password = "senzor12345"
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT broker!")
-        # Publish a message
-        topic = "senzor/console/log"
-        message = "{\"device\": \"your_device\", \"timestamp\": 123456789, \"sensor_top\": 420, \"sensor_bottom\": 69, \"angle\": 20.69}"
-        publish_message(topic, message)
     else:
         print("Failed to connect, return code %d" % rc)
 
 def on_message(client, userdata, msg):
     print("Received message: %s" % msg.payload.decode())
-
-def publish_message(topic, message):
-    client.publish(topic, message)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -30,10 +23,12 @@ client.username_pw_set(username, password)
 
 client.connect(broker_address, broker_port)
 
+def publish_message(topic, message):
+    client.publish(topic, message)
+
 def subscribe_topic(topic):
     client.subscribe(topic)
 
-publish_message("senzor/console/log", "{\"device\": \"your_device\", \"timestamp\": 123456789, \"sensor_top\": 420, \"sensor_bottom\": 69, \"angle\": 20.69}")
 subscribe_topic("B8:D6:1A:43:88:A8/data")
 
 client.loop_forever()

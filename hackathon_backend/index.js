5a907e8e16d67766d6d11ca167456b3bcd787aa7
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-16 12:57:11 +0200

    Update index.js

--- hackathon_backend/index.js
+++ hackathon_backend/index.js
@@ -23,6 +23,8 @@ const options = {
 
 const client  = mqtt.connect('mqtt://localhost:1883',options);
 
+
+
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
   client.subscribe('message', (err) => {
@@ -34,23 +36,14 @@ client.on('message', (topic, message) => {
   // message is a Buffer
   console.log(`Received message on ${topic}: ${message.toString()}`);
   
-  const msgObj = JSON.parse(message.toString());
-  const mac = msgObj.mac;
-  const angle = msgObj.angle;
-
   // Save the message to the database
-  const query = "INSERT INTO devices (mac, angle) VALUES (?, ?)";
-  db.query(query, [mac, angle], (err, result) => {
+  const query = "INSERT INTO devices (mac, angle) VALUES ('00:1B:44:11:3A:B7', 128.55)";
+  db.query(query, [message.toString()], (err, result) => {
     if (err) throw err;
     console.log('Message saved to the database');
   });
 });
 //test message
 client.on('connect', () => {
-    const data = {
-        mac: "00:1B:44:99:99:99",
-        angle: 45.85
-    };
-
-    client.publish('message', JSON.stringify(data));
-});
+    client.publish('message', 'Test message');
+  });

ff82ec8f90c15325a31d99e4a7a26b95ef8e8160
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-16 15:52:35 +0200

    backend test call

--- hackathon_backend/index.js
+++ hackathon_backend/index.js
@@ -3,7 +3,7 @@ const mqtt = require('mqtt');
 
 // Database Setup
 const db = mysql.createConnection({
-    host: '192.168.103.219',
+    host: 'localhost',
     user: 'root',
     port: 3306,
     password: 'ZlabSuckDick',
@@ -24,7 +24,7 @@ const options = {
     password: 'senzor12345'
   };
 
-const client  = mqtt.connect('mqtt://192.168.103.219',options);
+const client  = mqtt.connect('mqtt://192.168.103.219:1884',options);
 
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
@@ -33,7 +33,7 @@ client.on('connect', () => {
   });
 });
 
-client.on('message', (topic, message) => {
+client.on('device', (topic, message) => {
   // message is a Buffer
   console.log(`Received message on ${topic}: ${message.toString()}`);
   
@@ -49,14 +49,22 @@ client.on('message', (topic, message) => {
   });
 });
 
-
-
-//test message
-// client.on('connect', () => {
-//     const data = {
-//         mac: "00:1B:44:99:99:89",
-//         angle: 69.69
-//     };
-
-//     client.publish('message', JSON.stringify(data));
-// });
+client.on('senzor/console/log', (topic, message) => {
+    // message is a Buffer
+    console.log("someone called me");
+    // console.log(`Received message on ${topic}: ${message.toString()}`);
+    
+    // const msgObj = JSON.parse(message.toString());
+    // const device = msgObj.device;
+    // const timestamp = msgObj.timestamp;
+    // const sensorTop = msgObj.sensor_top;
+    // const sensorBottom = msgObj.sensor_bottom;
+    // const angle = msgObj.angle;
+  
+    // // Save the message to the database
+    // const query = "INSERT INTO devices (device, timestamp, sensor_top, sensor_bottom, angle) VALUES (?, ?, ?, ?, ?)";
+    // db.query(query, [device, timestamp, sensorTop, sensorBottom, angle], (err, result) => {
+    //   if (err) console.log('Message could not be saved in the database');
+    //   console.log('Message saved to the database');
+    // });
+  });

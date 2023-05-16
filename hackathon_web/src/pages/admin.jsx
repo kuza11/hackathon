76cbb770835f92e6f73b2791bb22aea629f35fff
Lukas Strnad <strnad.lukas@proton.me>
2023-05-16 13:30:28 +0000

    Website progress

---
+++ hackathon_web/src/pages/admin.jsx
@@ -0,0 +1,7 @@
+export default function Admin() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 text-2xl">
+      <div>Admin</div>
+    </main>
+  );
+}
---
+++ hackathon_web/src/pages/map.jsx
@@ -0,0 +1,7 @@
+export default function Map() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 text-2xl">
+      <div>Map</div>
+    </main>
+  );
+}

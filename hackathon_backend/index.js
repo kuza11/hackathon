3f08345a287f69e94ee80a59f21211d20e631a8f
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2023-05-16 22:45:31 +0200

    Update index.js

--- hackathon_backend/index.js
+++ hackathon_backend/index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
-const mqtt = require('mqtt');
-<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.1.0/paho-mqtt.min.js"></script>
+const mqtt = require('paho.mqtt.client');
+
 // Database Setup
 const db = mysql.createConnection({
     host: '127.0.0.1',
@@ -9,11 +9,6 @@ const db = mysql.createConnection({
     password: 'ZlabSuckDick',
     database: 'hackathon'
 });
-
-const client = new Paho.MQTT.Client(brokerAddress, brokerPort, "clientId");
-
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
 
 db.connect(error => {
     if (error) {

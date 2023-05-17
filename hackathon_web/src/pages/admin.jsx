ff5b82a791373f94538a10f3d30a2da045880ee4
Lukas Strnad <strnad.lukas@proton.me>
2023-05-17 02:53:42 +0000

    Admin page

--- hackathon_web/src/pages/admin.jsx
+++ hackathon_web/src/pages/admin.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export default function Admin() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 text-2xl">
@@ -8,7 +10,22 @@ export default function Admin() {
 }
 
 function Logs() {
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/live")
+      .then((res) => res.json())
+      .then((data) => setLogs(data));
+  }, [setLogs]);
+
+  function objToLog(obj) {
+    return `[${obj.time}]: top sensor: ${obj.sens_top}, bottom sensor: ${obj.sens_bottom}, at angle: ${obj.angle}`;
+  }
+
   return (
-    <div>test</div>
+    <>
+      <h1>Live logs from sensors</h1>
+      <textarea>{objToLog(logs)}</textarea>
+    </>
   );
 }
